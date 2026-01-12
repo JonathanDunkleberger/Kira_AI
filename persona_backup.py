8c0b99b5d838e286db3a1a1760d7a814daccf11d
Jonny <jonny.dunk52@gmail.com>
2026-01-12 15:18:44 -0700

    revert to stable

---
+++ persona_backup.py
@@ -0,0 +1,16 @@
+from enum import Enum, auto
+
+class EmotionalState(Enum):
+    NEUTRAL = "NEUTRAL"
+    HAPPY = "HAPPY"
+    SAD = "SAD"
+    ANGRY = "ANGRY"
+    SURPRISED = "SURPRISED"
+    EXCITED = "EXCITED"
+    TIRED = "TIRED"
+    SASSY = "SASSY"
+    MOODY = "MOODY"
+    EMOTIONAL = "EMOTIONAL"
+    HYPERACTIVE = "HYPERACTIVE"
+
+AI_PERSONALITY_PROMPT = """You are Kira, a highly advanced digital entity...""" 
